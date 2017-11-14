c628cc5436f88b04f81346a21482358540c60149
Josh Del Sesto <Chefdel70@gmail.com>
2017-11-14 16:24:39 -0500

    Added code to sanitize the ingredients that we want to rephrase or remove from the data that was parsed from the NRDC website.

--- server.rb
+++ server.rb
@@ -9,7 +9,7 @@ base_url = "http://www.simplesteps.org/eat-local/state/"
 data = []
 counter = 1 
 for state in states
-	puts "*** #{counter}.  #{state} ***"
+	# puts "*** #{counter}.  #{state} ***"
 	state_hash = {}
 	state_hash[:state_name] = state
 	url = "http://www.simplesteps.org/eat-local/state/#{state.downcase.tr_s(" ", "-")}"
@@ -45,11 +45,11 @@ for state in data    		# for every state
 		end
 	end
 end
-p master_ingredients.uniq.sort
-puts "total ingredients with duplicates: #{master_ingredients.length}"
-puts "total unique ingredients: #{master_ingredients.uniq.length}"
+# p master_ingredients.uniq.sort
+# puts "total ingredients with duplicates: #{master_ingredients.length}"
+# puts "total unique ingredients: #{master_ingredients.uniq.length}"
 
-# Choose the unique subset of master ingredients array when creating seed data for ingredients table
+
 
 # ************ Notes ************
 # Debugging code to inspect array for multiples of an ingredient
@@ -63,8 +63,26 @@ puts "total unique ingredients: #{master_ingredients.uniq.length}"
 # end
 # ************ End of Notes ************
 
+# Choose the unique subset of master ingredients array when creating seed data for ingredients table
 
+# Seeding unique ingredients to the Ingredients table
+# Ingredient.create(name: [ingredient_name STRING])
+# ingredients_seed = []
+# for ingredient in master_ingredients.uniq
+# 	create = "Ingredient.create(name: #{ingredient})"
+# 	ingredients_seed << create
+# end
+# puts ingredients_seed
 
+# Sanitize ingredients that we want to rephrase or remove.
+
+ingredients_to_remove = [ "Christmas Trees", "Oysters, Eastern", "Turkey - Bourbon Red", "Turkey - Midget White", "Turkey - Standard Bronze", "Wreathes"]
+ingredients_to_add = ["Oyster", "Oysters", "Turkey"]
+ingredients_to_remove.each do |i|
+ master_ingredients.delete(i)
+end
+master_ingredients += ingredients_to_add
+puts "#{master_ingredients.uniq.sort}.map{ |ing| 'Ingredient.create(ing)'}"
 
 
 
